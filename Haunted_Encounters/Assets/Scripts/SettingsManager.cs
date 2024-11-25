ea000523579449ed5f974b6def73de243f2f4b4a
CrossXT <158286713+CrossXT@users.noreply.github.com>
2024-11-25 19:49:47 +0100

    Update
    Mejorado UI de Settings,con sliders i script de guardado i cargado de ajustes

--- Haunted_Encounters/Assets/Scripts/SettingsManager.cs
+++ Haunted_Encounters/Assets/Scripts/SettingsManager.cs
@@ -1,18 +1,39 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class SettingsManager : MonoBehaviour
+public class SettingsMenu : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public Slider volumeSlider;
+    public Toggle fullscreenToggle;
+
     void Start()
     {
-        
+        // Cargar configuraciones guardadas
+        volumeSlider.value = PlayerPrefs.GetFloat("Volume", 1.0f);
+        fullscreenToggle.isOn = PlayerPrefs.GetInt("Fullscreen", 1) == 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    public void SetVolume(float volume)
     {
-        
+        AudioListener.volume = volume; // Cambiar volumen global
+        PlayerPrefs.SetFloat("Volume", volume); // Guardar
     }
+
+    public void SetFullscreen(bool isFullscreen)
+    {
+        Screen.fullScreen = isFullscreen;
+        PlayerPrefs.SetInt("Fullscreen", isFullscreen ? 1 : 0); // Guardar
+    }
+
+    public void ResetToDefaults()
+    {
+        SetVolume(1.0f); // Volumen predeterminado
+        SetFullscreen(true); // Pantalla completa predeterminada
+
+        volumeSlider.value = 1.0f;
+        fullscreenToggle.isOn = true;
+
+        Debug.Log("Valores restablecidos a predeterminados.");
+    }
+
 }
